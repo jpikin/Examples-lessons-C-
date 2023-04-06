96052b3307964979fc517f70ed3309c970100765
j.pikin <pikin@bk.ru>
2023-04-06 15:51:02 +0500

    Example005 if added

---
+++ Example005_helloPersonality/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите своё имя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "оля")
+    {
+        Console.WriteLine("Рады тебя видеть, Оля!");
+    }
+else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
