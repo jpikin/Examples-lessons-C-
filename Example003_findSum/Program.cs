6e429d38d9f2b3656039ce17b6671f8ceb05ec44
j.pikin <pikin@bk.ru>
2023-04-06 15:37:00 +0500

    random numbers to Example003 added

--- Example003_findSum/Program.cs
+++ Example003_findSum/Program.cs
@@ -1,4 +1,8 @@
-﻿int a = 31;
-int b = 1;
+﻿int a =  new Random().Next(1,10);
+int b =  new Random().Next(1,10);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
 Console.Write("Сумма этих чисел равна ");
 Console.WriteLine(a + b);
