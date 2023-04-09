96ba109db42172e505c90edb171c9c44040eee63
Evgeniy Pikin <pikin@bk.ru>
2023-04-09 16:07:35 +0500

    add Max function

--- Example008/Program.cs
+++ Example008/Program.cs
@@ -10,10 +10,10 @@ int a1 = 1, a2 = 2, a3 = 3,
     b1 = 4, b2 = 5, b3 = 6,
     c1 = 7, c2 = 8, c3 = 9;
 
-int max1 = Max(a1, a2, a3),
-    max2 = Max(b1, b2, b3),
-    max3 = Max(c1, c2, c3);
-
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, a2, a3),
+//     max2 = Max(b1, b2, b3),
+//     max3 = Max(c1, c2, c3);
+// int max = Max(max1, max2, max3);
+int max = Max(Max(a1,a2,a3), Max(b1,b2,b3), Max(c1,c2,c3));
 
 Console.WriteLine(max);
