ef01440c15d50d35a37de7f6e5bce21017d8e8bd
Evgeniy Pikin <pikin@bk.ru>
2023-04-09 17:10:11 +0500

    Example009 array_3 finish

--- Example009_array_3/Program.cs
+++ Example009_array_3/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection) // принимаем в качестве аргумента массив
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10); //  заполняем массив случайными числами
+        index++;
+    }
+}
+
+void PrintArray(int[] col) // принимаем в качестве аргумента массив
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);  // печатаем элемент массива под индексом position
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) // создаем метод для поиска индекса элемента массива
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // объявляем массив по умолчанию 10 элементов.
+
+FillArray(array); //передаем в метод наш массив, произойдет заполнение массива ранд числами
+PrintArray(array); // передаем в метод наш массив, метод будет печатать элементы массива по очереди
+Console.WriteLine(); // пустая строка
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
