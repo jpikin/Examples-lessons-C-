98bf8254ec976507cb84b2a18cc3e03ea58072c4
Evgeniy Pikin <pikin@bk.ru>
2023-04-09 16:38:36 +0500

    Example009 array_2 finfsh

--- Example009_array_2/Program.cs
+++ Example009_array_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {11, 21, 3, 44, 15, 6, 79, 30};
+
+int n = array.Length; // записываем в n длину массива
+
+int find = 6;
+
+int index = 0;
+
+while (index < n)
+    {
+        if (array[index] == find)
+        {
+            Console.WriteLine(index);
+            break;    
+        }
+        index++;
+    }
