04b2c42d6a7b4f04fd779d7b590be2a2ddb1a8b8
Evgeniy Pikin <pikin@bk.ru>
2023-04-09 16:21:07 +0500

    Example009 array first step

--- Example009_array/Program.cs
+++ Example009_array/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max (int arg1, int arg2, int arg3)
+    {
+        int result = arg1;
+        if (arg2 > result) result = arg2;
+        if (arg3 > result) result = arg3;
+        return result;
+    }
+
+int[] array = {11,21,33,44,15,6,77,18,9}; // объявляем массив
+
+int max = Max
+            (Max(array[0],array[1],array[2]),
+             Max(array[3],array[4],array[5]), 
+             Max(array[6],array[7],array[8])
+            );
+
+Console.WriteLine(max);
